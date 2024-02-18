63db7a10aaf17a2e27dbe349e1c8ac8ae43c05c2
Krzysiek <kubas.krzysztof@gmail.com>
2024-02-18 01:10:07 +0100

    nav mobile

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const navMobile = document.querySelector('.mobile-nav__items')
+const navBtn = document.querySelector('.hamburger')
+const allNavItem = document.querySelectorAll('.mobile-nav__nav-link')
+const handleNav = () => {
+	navBtn.classList.toggle('is-active')
+	navMobile.classList.toggle('active')
+
+	allNavItem.forEach(item => {
+		item.addEventListener('click', () => {
+			navMobile.classList.remove('active')
+		})
+	})
+
+	allNavItem.forEach(item => {
+		item.addEventListener('click', () => {
+			navBtn.classList.remove('is-active')
+		})
+	})
+}
+navBtn.addEventListener('click', handleNav)
